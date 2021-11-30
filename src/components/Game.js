16b0a76e545fe9108b262c8b413b0b8331870be9
razvanr91 <razvan.roman91@outlook.com>
2021-11-30 14:48:04 +0000

    Style homepage and add gamelist component

---
+++ src/components/Game.js
@@ -0,0 +1,27 @@
+import React from "react";
+// Styling and animations
+import styled from "styled-components";
+import { motion } from "framer-motion/dist/es";
+
+const StyledGame = styled(motion.div)`
+    min-height: 30vh;
+    box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+    text-align: center;
+    border-radius: 1rem;
+    img {
+        width: 100%;
+        height: 40vh;
+        object-fit: cover;
+    };
+
+`;
+
+export default function Game({ game }) {
+    return (
+        <StyledGame>
+            <h3>{game.name}</h3>
+            <p>{game.released}</p>
+            <img src={game.background_image} alt={game.name} />
+        </StyledGame>
+    );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,45 @@
 import React, { useEffect } from "react";
 // Redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+// Components
+import Game from "../components/Game";
+
+// Styling and animations
+import styled from "styled-components";
+import { motion } from "framer-motion/dist/es/index";
+
+const GameList = styled(motion.div)`
+    padding:0rem 5rem;
+    h2{
+        padding: 5rem 0rem;
+    }    
+`;
+const Games = styled(motion.div)`
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap: 5rem;
+`;
 
 const Home = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(loadGames());
-    })
+    }, [dispatch]);
+
+    // Extract data
+    const { popularGames, upcomingGames, newGames } = useSelector(state => state.games)
     return (
-        <h1>Home</h1>
+        <GameList>
+            <h2>Upcoing Games</h2>
+            <Games>
+                {upcomingGames.map(game => {
+                    return <Game key={game.id} game={game} />
+                })}
+            </Games>
+        </GameList>
     );
 }
 
